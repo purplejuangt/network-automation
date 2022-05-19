42972f0fde149902b81043bd185093e93c9e5afc
purplejuangt <88762182+purplejuangt@users.noreply.github.com>
2022-05-19 13:05:57 -0600

    Add files via upload

---
+++ Show-services.py
@@ -0,0 +1,48 @@
+from netmiko import ConnectHandler
+
+hosts = ['11.11.11.1', '192.168.122.195']
+
+for ip in hosts:
+
+    cisco_device = {
+        'device_type': 'cisco_ios',
+        'host':   ip,
+        'username': 'test',
+        'password': 'test',
+        'port' : 22,          # optional, defaults to 22
+        'secret': 'test',     # optional, defaults to ''
+    }
+
+    net_connect = ConnectHandler(**cisco_device)
+
+    sh_username = net_connect.send_command('show running-config | i username')
+    sh_snmp = net_connect.send_command('show running-config | i snmp')
+    sh_logging = net_connect.send_command('show running-config | i logging')
+    sh_ntp = net_connect.send_command('show running-config | i ntp')
+    sh_tacacs = net_connect.send_command('show running-config | i tacacs')
+    sh_aaa = net_connect.send_command('show running-config | i aaa')
+    sh_server = net_connect.send_command('show running-config | i server')
+    sh_linevty = net_connect.send_command('show running-config | b vty | i access-class')
+    sh_acl = net_connect.send_command('show access-list | b ssh')
+    print('Information for {} \n'.format(ip))
+    print('************usernames***************** \n')
+    print(sh_username)
+    print('*************snmp**************** \n')
+    print(sh_snmp)
+    print('**************Syslog************** \n')
+    print(sh_logging)
+    print('*************NTP config**************** \n')
+    print(sh_ntp)
+    print('**************TACACS*************** \n')
+    print(sh_tacacs)
+    print('**************AAA*************** \n')
+    print(sh_aaa)
+    print('**************Tac-servers*************** \n')
+    print(sh_server)
+    print('**************LineVTY*************** \n')
+    print(sh_linevty)
+    print('*************ManagmentACL**************** \n')
+    print(sh_acl)
+    print('***************************** \n')
+    print('***************************** \n')
+
